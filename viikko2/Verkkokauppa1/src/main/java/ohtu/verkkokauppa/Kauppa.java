e9e0bef5fe1d0bef1a3bac1393cd0e69f9c4c2db
Roeoeri <tomi.haapakoski@helsinki.fi>
2019-11-11 21:57:26 +0200

    viikko2 loput tehtävät

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -0,0 +1,47 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Kauppa {
+
+    private Sailytystila varasto;
+    private RahoitusLaitos pankki;
+    private Ostoskori ostoskori;
+    private Generaattori viitegeneraattori;
+    private String kaupanTili;
+
+    @Autowired
+    public Kauppa(Generaattori viite, RahoitusLaitos laitos, Sailytystila tila) {
+        varasto = tila;
+        pankki = laitos;
+        viitegeneraattori = viite;
+        kaupanTili = "33333-44455";
+    }
+
+    public void aloitaAsiointi() {
+        ostoskori = new Ostoskori();
+    }
+
+    public void poistaKorista(int id) {
+        Tuote t = varasto.haeTuote(id); 
+        varasto.palautaVarastoon(t);
+    }
+
+    public void lisaaKoriin(int id) {
+        if (varasto.saldo(id)>0) {
+            Tuote t = varasto.haeTuote(id);             
+            ostoskori.lisaa(t);
+            varasto.otaVarastosta(t);
+        }
+    }
+
+    public boolean tilimaksu(String nimi, String tiliNumero) {
+        int viite = viitegeneraattori.uusi();
+        int summa = ostoskori.hinta();
+        
+        return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
+    }
+
+}
